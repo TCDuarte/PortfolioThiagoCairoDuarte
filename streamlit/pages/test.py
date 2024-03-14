01ecd447fbfc6c741dbe1a3a2f8590c82d43a169
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 19:06:11 -0300

    Update test.py

--- streamlit/pages/test.py
+++ streamlit/pages/test.py
@@ -7,5 +7,10 @@ with st.sidebar:
         # icons=['house', 'gear'], 
         menu_icon="cast", default_index=1)
     selected
-selected
-print (selected)
+    
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
+
+print (selected2)
