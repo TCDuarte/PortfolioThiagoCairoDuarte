d33dbb4c738d4426f976deaf97624582553505b7
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 19:07:18 -0300

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -3,7 +3,14 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected = option_menu("Linguagem", ["Português", 'English'], 
+    selected = option_menu("Idioma", ["Português", 'English'], 
         # icons=['house', 'gear'], 
         menu_icon="cast", default_index=1)
     selected
+    
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
+
+print (selected2)
