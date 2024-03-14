621e70420e543bda23afc52416980df7f0d4a4dd
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 18:59:05 -0300

    Update test.py

--- streamlit/pages/test.py
+++ streamlit/pages/test.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu("Linguagem", ["Português", 'English'], 
+        # icons=['house', 'gear'], 
+        menu_icon="cast", default_index=1)
+    selected
