8c2038ba7d8e2b3d7cb9bc2f1f0e9c2bfd809834
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 19:04:57 -0300

    Update test.py

--- streamlit/pages/test.py
+++ streamlit/pages/test.py
@@ -8,19 +8,4 @@ with st.sidebar:
         menu_icon="cast", default_index=1)
     selected
     
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-selected2
-
-if st.session_state.get('switch_button', False):
-    st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
-    manual_select = st.session_state['menu_option']
-else:
-    manual_select = None
-    
-selected4 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    orientation="horizontal", manual_select=manual_select, key='menu_4')
-st.button(f"Move to Next {st.session_state.get('menu_option', 1)}", key='switch_button')
-selected4
+print (selected)
