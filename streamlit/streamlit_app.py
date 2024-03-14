57160d9008b484e35f42d711b8b1cb0cdab822e7
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 19:21:13 -0300

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -9,9 +9,11 @@ with st.sidebar:
     
 selected2 = option_menu(None, ["Currículo", "Portifólio", "Certificados", "Sobre"], 
     # icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    on_change = st.write(1))
 
 var = str(selected2)
+st.write(var)
 if var == 'Currículo':
     print('Salve')
 else: print ('Não salve')
