142a061aeb2009117b9a039be6cf602880a2609a
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 18:48:08 -0300

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
