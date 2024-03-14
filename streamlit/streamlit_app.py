1845bc8fb351499a61df3c947d21215d5427c8b3
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 19:33:21 -0300

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -5,18 +5,13 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected = option_menu("Idioma", ["Português", 'English'], 
+    selectedLanguage = option_menu("Idioma", ["Português", 'English'], 
         # icons=['house', 'gear'], 
         menu_icon="cast", default_index=1)
     
 selected2 = option_menu(None, ["Currículo", "Portifólio", "Certificados", "Sobre"], 
-    # icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    icons=['file-earmark-text', 'clipboard-data', "patch-check", 'info-lg'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
-
-
-
-# var = str(selected2)
-# st.write(var)
 
 ########## CURRÍCULO ##########
 if selected2 == 'Currículo':
