04cf75ffb901bb19f97aecd878c9edf25f0c8159
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-14 20:49:28 -0300

    Update streamlit

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -4,6 +4,46 @@ import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 import requests
+import base64
+
+########## DECLARAÇÃO DAS VARIÁVEIS CHAVES ##########
+
+username = 'TCDuarte'
+repository_name = 'PortfolioThiagoCairoDuarte'
+
+########## DECLARAÇÃO DAS FUNÇÕES ##########
+
+def get_file_from_github(file_path, pat=None):
+  """
+  Função para abrir os arquivos de texto do repositório.
+  Com o objetivo de não sobrecarregar o código com textos, os mesmos foram salvos dentro do repositório.
+  Por se tratar de um repositório público, não necessita de uma etapa de autenticação.
+
+  Argumentos:
+      file_path (str): Caminho para o arquivo.
+
+  Returns:
+      str: Texto contido dentro do arquivo.
+  """
+
+  # URL da API baseada nos demais parâmetros
+  url = f"https://api.github.com/repos/{username}/{repository_name}/contents/{file_path}"
+
+  # Request
+  response = requests.get(url)
+
+  # Checa se a request obteve sucesso
+  if response.status_code == 200:
+    data = response.json()
+    
+    # Decodificação do arquivo em texto
+    file_content = base64.b64decode(data["content"]).decode("utf-8")
+    return file_content
+  else:
+    print(f"Error: {response.status_code}")
+    return None
+
+########## CONFIGURAÇÃO DO STREAMLIT ##########
 
 with st.sidebar:
     selectedLanguage = option_menu('Idioma', ['Português', 'English'], 
@@ -14,59 +54,17 @@ selected2 = option_menu(None, ['Currículo', 'Portifólio', 'Certificados', 'Sob
     icons=['file-earmark-text', 'clipboard-data', 'patch-check', 'info-lg'], 
     menu_icon='cast', default_index=0, orientation='horizontal')
 
-
-username = 'TCDuarte'
-repository_name = 'PortfolioThiagoCairoDuarte'
-
-def get_file_from_github(file_path, pat=None):
-  """
-  This function retrieves the content of a file from a public GitHub repository using the GitHub API.
-
-  Args:
-      username (str): The username of the repository owner.
-      repository_name (str): The name of the repository.
-      file_path (str): The path to the file within the repository.
-      pat (str, optional): A Personal Access Token for authentication (optional for public repos).
-
-  Returns:
-      str: The content of the file (decoded from base64), or None if unsuccessful.
-  """
-
-  # Build the API URL
-  url = f"https://api.github.com/repos/{username}/{repository_name}/contents/{file_path}"
-
-  # Set up authentication if provided
-  auth = (username, pat) if pat else None
-
-  # Send the request
-  response = requests.get(url, auth=auth)
-
-  # Check for successful response
-  if response.status_code == 200:
-    # Parse the JSON response
-    data = response.json()
-    
-    # Access and decode the file content
-    return data["content"].encode("utf-8").decode("base64")
-  else:
-    print(f"Error: {response.status_code}")
-    return None
-
-
-
-
-
-########## CURRÍCULO ##########
+########## ABA - CURRÍCULO ##########
 if selected2 == 'Currículo':
     st.write('Salve')
     
-########## PORTIFÓLIO ##########   
+########## ABA - PORTIFÓLIO ##########   
 elif selected2 == 'Portifólio':
     st.write('Salve2')
     
-########## CERTIFICADOS ##########
+########## ABA - CERTIFICADOS ##########
 elif selected2 == 'Certificados':
     st.write('Salve3')
     
-########## SOBRE ##########
+########## ABA - SOBRE ##########
 else: st.write ('Não salve')
