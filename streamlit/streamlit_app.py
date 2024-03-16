bfbf049680061e7945174055801927409fb1392c
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-16 16:56:09 -0300

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -63,7 +63,7 @@ with st.sidebar:
     st.write('thiago.cairo.duarte@gmail.com')
     st.write('https://www.linkedin.com/in/tcduarte/')
     selectedLanguage = option_menu('Idioma', ['Português', 'English'],
-        menu_icon='cast', default_index=1)
+        menu_icon='cast', default_index=0)
     
 options = ['Currículo', 'Portifólio', 'Certificados', 'Sobre'] if selectedLanguage == 'Português' else ['Curriculum', 'Portfolio', 'Certificates', 'About']
 
