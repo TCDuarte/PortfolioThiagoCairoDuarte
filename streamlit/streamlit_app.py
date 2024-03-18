476e75d8bab1308547ca168b5d1d9bce106614a8
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-18 17:56:41 -0300

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -87,8 +87,8 @@ elif selected2 == options[1]:
     with col1:
         def example():
             card(
-                title="Hello World!",
-                text="",
+                title="",
+                text="Inclusor de certificações",
                 image="https://github.com/TCDuarte/PortfolioThiagoCairoDuarte/blob/main/streamlit/imgs/portifolio_pictures/cert_includ.png?raw=true",
                 url="https://docs.google.com/spreadsheets/d/12lkSbB5lPSyyEMMUkw35cFwOrVYnq3k2NVO11-6oF8g/edit#gid=0",
             )
