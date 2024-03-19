9a8e4c7230da99b059f41bf04e6e69363f076f61
TCDuarte <77121233+TCDuarte@users.noreply.github.com>
2024-03-19 20:35:14 -0300

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -73,7 +73,7 @@ def add_to_portfolium_page(data, number):
                     "width": "250px",
                     "height": "350px",
                     "border-radius": "30px",
-                    "align-items": "none"}
+                    "display": "infinite"}
                     }
                 )
         tagger_component(
